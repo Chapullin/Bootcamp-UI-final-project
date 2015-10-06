e00430aacf3e62a7366a9374514daa932ff31368
Chapullin <molinero.cristian.d@gmail.com>
2015-10-06 11:09:20 -0300

    checking routes

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -1,17 +1,40 @@
 var app = angular.module('myApp', []);
 var date;
 
-console.log('BEFORE trying conect with the server');
-
 app.controller('TwitterServerCtrl', function($scope, $http) {
 	$http.get("http://localhost:3000/timeline")
 	.success(function (response) {
 		$scope.names = response;
-		console.log('inside de function');
+		
 	});
 
 	$scope.date = ((new date()) - $scope.names.user.created_at);
-});
+}).
+	controller('TrendsCtrl', function ($scope, $scope.names) {
+		
+	}
 
-console.log('AFTER trying conect with the server');
+		)
+	;
 
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: '/trends.html',
+        controller: 'TrendsCtrl'
+      }).
+      when('/movies/:id', {
+        templateUrl: 'app/partial/movie-detail.html',
+        controller: 'MovieDetailsCtrl'
+      }).
+      when('/edit/:id', {
+        templateUrl: 'app/partial/movie-form.html',
+        controller: 'MovieEditCtrl'
+      }).
+      
+      otherwise({
+        redirectTo: '/'
+      });
+  }]); 
