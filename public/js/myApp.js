7edd14ad6047df30ee9d318b46c9e33fdb55613a
Chapullin <molinero.cristian.d@gmail.com>
2015-10-06 16:47:58 -0300

    error cannont read properly 0

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -1,40 +1,48 @@
-var app = angular.module('myApp', []);
-var date;
+var app = angular.module('myApp', ['ngRoute']);
 
-app.controller('TwitterServerCtrl', function($scope, $http) {
+
+app.controller('TimeLineCtrl',  function ($scope, $http) {
 	$http.get("http://localhost:3000/timeline")
 	.success(function (response) {
-		$scope.names = response;
-		
+		$scope.timeline = response;
+	})}).
+
+	controller('TwittDetailCtrl',  function ($scope, $http, $routeParams) {
+		$http.get("http://localhost:3000/timeline?count=100")
+		.success(function (response, $scope, $routeParams) {
+			$scope.timeline = response;
+			$scope.id = $routeParams.id;
+			for (var i = 0; i < 100; i++) {
+				if ($scope.timeline[i].id === $routeParams.id) {
+					$scope.twitt = $scope.timeline[i];
+				}};
+			})}).
+
+	controller('TrendsCtrl',  function ($scope, $http, $routeParams) {
+		$http.get("http://localhost:3000/trends?id=23424747")
+		.success(function (response, $scope, $routeParams) {
+			$scope.trends = response;
+			})
 	});
 
-	$scope.date = ((new date()) - $scope.names.user.created_at);
-}).
-	controller('TrendsCtrl', function ($scope, $scope.names) {
-		
-	}
 
-		)
-	;
+	app.config(['$routeProvider',
+		function($routeProvider) {
+			$routeProvider.
+			when('/', {
+				templateUrl: '/timeline.html',
+				controller: 'TimeLineCtrl'
+      }). 
+			when('/:id', {
+				templateUrl: '/twitt-detail.html',
+				controller: 'TwittDetailCtrl'
+			}).
+			when('/trends', {
+				templateUrl: '/trends.html',
+				controller: 'TrendsCtrl'
+			}).
 
-
-app.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: '/trends.html',
-        controller: 'TrendsCtrl'
-      }).
-      when('/movies/:id', {
-        templateUrl: 'app/partial/movie-detail.html',
-        controller: 'MovieDetailsCtrl'
-      }).
-      when('/edit/:id', {
-        templateUrl: 'app/partial/movie-form.html',
-        controller: 'MovieEditCtrl'
-      }).
-      
-      otherwise({
-        redirectTo: '/'
-      });
-  }]); 
+			otherwise({
+				redirectTo: '/'
+			});
+		}]); 
