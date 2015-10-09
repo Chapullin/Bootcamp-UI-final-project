788714cfe70b508f7deedabf13113905e8c9a71e
Chapullin <molinero.cristian.d@gmail.com>
2015-10-09 09:08:42 -0300

    twit detail inserted in timeline for less request to server

--- public/js/controller/controllers.js
+++ public/js/controller/controllers.js
@@ -6,100 +6,102 @@ app.controller('TimeLineCtrl', ['$scope', '$routeParams', '$location', 'ajax', f
 			url: 'http://localhost:3000/timeline',
         }, function (data) { // callback
         	$scope.loading = false;
-        	if (data) {
-        		$scope.timeline= data;
-        	}
-        });
-	})();
-	$scope.go = function ( path ) {
-		$location.path( path );
-		console.log('path = ',path);
-	};
-}]).
-
-controller('TwittDetailCtrl',['$scope', '$routeParams', 'ajax',function ($scope, $routeParams, AJAX){
-	console.log('controller TwittDetailCtrl------------------------------');
-	var timeline = (function () {
-		$scope.loading = true;
-		AJAX.query({
-			url: 'http://localhost:3000/timeline',
-        }, function (data) { // callback
-        	$scope.loading = false;
-        	if (data) {
-        		$scope.timeline= data;
-        		$scope.id = $routeParams.id;
-	       		for (var i = 0; i < data.length; i++) {
-        			if (data[i].id == $routeParams.id) {
-        				$scope.twitt = data[i];
-        				
-        			}};
-        		}
-        	});
-	})();
-}]).
-
-
-controller('TrendsListCtrl', ['$scope', '$routeParams',  'ajax', function ($scope, $routeParams, AJAX){
-	console.log('under TrendsCtrl <<<<<<<<<<<<<<<<<<<');
-	var trends = (function () {
-		$scope.loading = true;
-
-		AJAX.query({
-			url: 'http://localhost:3000/trends?id=23424747',
-        }, function (data) { // callback
-        	$scope.loading = false;
-         	if (data) {
-        		$scope.trends= data;
-        	}
-        });
-	})();
-}]).
-
-controller('TrendsSearchCtrl',['$scope', '$routeParams', 'ajax',function ($scope, $routeParams, AJAX){
-	console.log('controller TrendsListCtrl------------------------------');
-	var trend = (function () {
-		$scope.loading = true;
- 		AJAX.query({
-			url: ('http://localhost:3000/search?q='+ $routeParams.query),
-        }, function (data) { // callback
-        	console.log('con + http://localhost:3000/search?q='+ $routeParams.query);
-        	$scope.loading = false;
-        	if (data) {
-        		 $scope.id = $routeParams.id;
-	       		$scope.trend = data;   
-        		}
-        	});
-	})();
-}]).
-
-controller('TrendTwitDetailCtrl',['$scope', '$routeParams', 'ajax',function ($scope, $routeParams, AJAX){
-    console.log('controller TrendTwitDetailCtrl ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-    var trend = (function () {
-        console.log('$routeParams.query= ',$routeParams.query);
-        $scope.loading = true;
-        console.log('$routeParams.query= ',$routeParams.query);
-        window.alert('no estamos todos localhost');
-        AJAX.query({
-            url: ('http://localhost:3000/search?q='+ $routeParams.query),
-        }, function (data) { // callback
-            $scope.loading = false;
+            console.log('data= ', data);
             if (data) {
-                $scope.trend= data;
-                window.alert('mira la console guapeton!!!');
-                console.log('$routeParams= ', $routeParams);
-                $scope.id = $routeParams.id;
-                for (var i = 0; i < data.length; i++) {
-                    window.alert('mira la console guapeton!!!');
-                    console.log('data= ', data);
-                    if (data[i].id == $routeParams.id) {
-                        $scope.twitt = data[i];
-                        
-                    }};
-                }
-            });
-    })();
-}])
+              $scope.timeline= data;
+          }
+      });
+	})();
+    }]).
+
+    controller('TrendsListCtrl', ['$scope', '$routeParams', 'ajax', 'geolocalisation', function ($scope, $routeParams, AJAX, GEOLOCALISATION){
+       console.log('under TrendsCtrl <<<<<<<<<<<<<<<<<<<');
+       var geo = function (){
+            GEOLOCALISATION.getUserCoords(function (coordinates) {
+              //http://localhost:3000/myplace?lat=-38.7116780&long=-62.2680780
+              $scope.msgCoordinates = coordinates
+                                  ? 'lat=' + coordinates.coords.latitude + '\&long=' + coordinates.coords.longitude
+                                  : 'Coordinates couldn\'t be determined.';
+              $scope.$apply(); // assure that $scope changes are applied to the view
+          });
+        };
+
+        $scope.geo = geo;
+        $scope.coordsAvailable = GEOLOCALISATION.canGetCoords;
+       
+
+        if ($scope.coordsAvailable) {
+            AJAX.query({
+             url: ('http://localhost:3000/myplace?'+ $scope.msgCoordinates),
+                }, function (data) { // callback
+                  if (data) {
+                      $scope.idCountry = data.woeid;
+                  }
+              });
+        } else{
+            $scope.idCountry = 23424747;
+        };
+
+        
 
 
+
+       var trends = (function () {
+          $scope.loading = true;
+
+          AJAX.query({
+             url: ('http://localhost:3000/trends?id='+ $scope.idCountry),
+                }, function (data) { // callback
+                	$scope.loading = false;
+                  if (data) {
+                      $scope.trends= data;
+                  }
+              });
+      })();
+    }]).
+
+    controller('TrendsSearchCtrl',['$scope', '$routeParams', 'ajax',function ($scope, $routeParams, AJAX){
+       console.log('controller TrendsListCtrl------------------------------');
+       var trend = (function () {
+          $scope.loading = true;
+          AJAX.query({
+             url: ('http://localhost:3000/search?q='+ $routeParams.query),
+                }, function (data) { // callback
+                	console.log('con + http://localhost:3000/search?q='+ $routeParams.query);
+                	$scope.loading = false;
+                	if (data) {
+                     $scope.id = $routeParams.id;
+                     $scope.trend = data;   
+                 }
+             });
+      })();
+    }]).
+
+    controller('TrendTwitDetailCtrl',['$scope', '$routeParams', 'ajax',function ($scope, $routeParams, AJAX){
+        console.log('controller TrendTwitDetailCtrl ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+        var trend = (function () {
+            console.log('$routeParams.query= ',$routeParams.query);
+            $scope.loading = true;
+            console.log('$routeParams.query= ',$routeParams.query);
+            window.alert('no estamos todos localhost');
+            AJAX.query({
+                url: ('http://localhost:3000/search?q='+ $routeParams.query),
+                }, function (data) { // callback
+                    $scope.loading = false;
+                    if (data) {
+                        $scope.trend= data;
+                        window.alert('mira la console guapeton!!!');
+                        console.log('$routeParams= ', $routeParams);
+                        $scope.id = $routeParams.id;
+                        for (var i = 0; i < data.length; i++) {
+                            window.alert('mira la console guapeton!!!');
+                            console.log('data= ', data);
+                            if (data[i].id == $routeParams.id) {
+                                $scope.twitt = data[i];
+                                
+                            }};
+                        }
+                    });
+        })();
+    }])
 ;
-
--- public/js/myApp.js
+++ public/js/myApp.js
@@ -2,31 +2,26 @@ var app = angular.module('myApp', ['ngRoute']);
 
 
 app.config(['$routeProvider',
-		function($routeProvider) {
-			$routeProvider.
-			when('/', {
-				templateUrl: '/timeline.html',
-				controller: 'TimeLineCtrl'
-      }). 
-			when('/twit/:id', {
-				templateUrl: '/twitt-detail.html',
-				controller: 'TwittDetailCtrl'
-			}).
-			when('/trends', {
-				templateUrl: '/trends.html',
-				controller: 'TrendsListCtrl'
-			}).
-			when('/search/trends/:query', {
-				templateUrl: '/trends-search.html',
-				controller: 'TrendsSearchCtrl'
-			}).
-			when('/search/trends/twit/:query', {
-				templateUrl: '/trend-tweet-detail.html',
-				controller: 'TrendTwitDetailCtrl'
-			}).
-			otherwise({
-				redirectTo: '/'
-			});
-		}]); 
+	function($routeProvider) {
+		$routeProvider.
+		when('/', {
+			templateUrl: '/timeline.html',
+			controller: 'TimeLineCtrl'
+		}). 
+		when('/trends', {
+			templateUrl: '/trends.html',
+			controller: 'TrendsListCtrl'
+		}).
+		when('/search/trends/:query', {
+			templateUrl: '/trends-search.html',
+			controller: 'TrendsSearchCtrl'
+		}).
+		when('/search/trends/twit/:query', {
+			templateUrl: '/trend-tweet-detail.html',
+			controller: 'TrendTwitDetailCtrl'
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+	}]); 
 
-	
