72dae14d0af90364e81b88123ec21c447bda9ca8
Chapullin <molinero.cristian.d@gmail.com>
2015-10-09 12:45:05 -0300

    geolocalizating i hate you

--- public/js/factory/factories.js
+++ public/js/factory/factories.js
@@ -21,25 +21,30 @@ app.factory('ajax', function ($http) {
 
   .factory('geolocalisation', function () {
       var coords,
-      getUserCoords = function () { 
+      getUserCoords = function (cb) { 
         console.log('we are inside of function getUserCoords ' );
             if (!coords && navigator.geolocation) { // if coords haven't been determined yet and if browser allows to get them
               console.log(' we are inside of the if !coords && navigator.geolocation' );
               navigator.geolocation.getCurrentPosition(function (pos) { // user shared his coords
                 onUserChoice(pos);
+                console.log('ohhhsole mio');
+                console.log('pos=', pos);
               }, function () { // user didn't share his coords
               onUserChoice(cb);
-            });
+              console.log('ohhhsole mio 2');
+            }); 
             } else { // else, if coords are already determined or browser doesn't allow to get them
             cb(coords);
+            window.alert('uops!! coords are already determined or browser doesnt allow to get them');
           }
         },
         onUserChoice = function (pos) {
+          console.log('we are inside of onUserChoice');
           if (pos) {
+            console.log('we are inside the if');
             coords = pos;
+            console.log('coords = ', coords);
           }
-
-          cb(coords);
         };
 
         return {
